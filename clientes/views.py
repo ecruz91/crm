5c7ce74dc670d00f8e0e089615f994691f813f30
ecruz91 <e.cruz@codelab.mx>
2016-07-03 04:19:43 -0500

    modified:   clientes/views.py

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth import logout, authenticate, login
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.template.loader import get_template
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,10 +9,10 @@ def clientes(request):
 	if request.user.is_authenticated():
 		return render(request, 'clientes/clientes.html', {})
 	else:
-		return HttpResponseRedirect("/login/")
+		return HttpResponseRedirect("/")
 
 def cliente_nuevo(request):
 	if request.user.is_authenticated():
 		return render(request, 'clientes/nuevo.html', {})
 	else:
-		return HttpResponseRedirect("/login/")
+		return HttpResponseRedirect("/")
